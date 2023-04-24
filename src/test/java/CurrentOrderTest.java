2930c7522b90527bb28dc00330275285f4672dd4
2773246D <2773246d@student.gla.ac.uk>
2023-04-24 22:42:50 +0100

    Made use of the previous configuration and added two mock tests for the CurrentOrder class

--- src/test/java/CurrentOrderTest.java
+++ src/test/java/CurrentOrderTest.java
@@ -1,7 +1,4 @@
-
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -10,7 +7,6 @@ public class CurrentOrderTest {
     @Test
     public void testCurrentOrderValueMock(){
 
-
         CurrentOrder mockCurrentOrder = mock(CurrentOrder.class);
         double productPrice = mockCurrentOrder.initaliseCurrentOrder();
         when(mockCurrentOrder.currentOrderValue(productPrice)).thenReturn(0.0);
@@ -18,5 +14,16 @@ public class CurrentOrderTest {
         assertEquals(0.0, currentOrderValue);
 
     }
+    @Test
+    public void testOutputCurrentOrderMock(){
+
+        CurrentOrder mockCurrentOrder = mock(CurrentOrder.class);
+        double currentOrderValue = mockCurrentOrder.initaliseCurrentOrder();
+        when(mockCurrentOrder.outputCurrentOrder()).thenReturn("0.0");
+        String output = mockCurrentOrder.outputCurrentOrder();
+        assertEquals("0.0", output);
+
+    }
+
 
 }
