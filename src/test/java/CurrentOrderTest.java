f6a317e43a84893ab01de4a77e6f79e4934d79ec
2773246D <2773246d@student.gla.ac.uk>
2023-04-24 21:54:20 +0100

    Configured the project so that we can now run mocks.

--- src/test/java/CurrentOrderTest.java
+++ src/test/java/CurrentOrderTest.java
@@ -1,9 +1,22 @@
 
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class CurrentOrderTest {
 
+    @Test
+    public void testCurrentOrderValueMock(){
+
+
+        CurrentOrder mockCurrentOrder = mock(CurrentOrder.class);
+        double productPrice = mockCurrentOrder.initaliseCurrentOrder();
+        when(mockCurrentOrder.currentOrderValue(productPrice)).thenReturn(0.0);
+        double currentOrderValue = mockCurrentOrder.currentOrderValue(productPrice);
+        assertEquals(0.0, currentOrderValue);
+
+    }
 
 }
